e256b93209b6cbf90616362723dc2efc5dd8bfb4
Vinicius Barreto <viniciusbarretodasilva84@gmail.com>
2025-11-21 08:16:35 -0300

    Adiciona classe Pagamento ao app orderitems

--- estacionamentoapp/apps/orderitems/models.py
+++ estacionamentoapp/apps/orderitems/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from products.models import Product
-from orders.models import Order
+from orders.models import Order, Reserva
+
 # Create your models here.
 class Orderitem(models.Model):
     quantity = models.PositiveIntegerField('Quantidade',null=True, blank=True,default=0)
@@ -16,3 +17,23 @@ class Orderitem(models.Model):
 
     def __str__(self):
         return f"{self.quantity} - {self.product.name}"
+    
+class Pagamento(models.Model):
+    pagamento_id = models.AutoField(primary_key=True)
+    tipo_metodo = models.CharField(max_length=30, choices=[
+        ('pix', 'PIX'),
+        ('cartao', 'Cartão de Crédito'),
+        ('boleto', 'Boleto'),
+    ])
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+    data_pagamento = models.DateTimeField(auto_now_add=True)
+    aprovado = models.BooleanField(default=False)
+
+    reserva = models.ForeignKey(
+        Reserva,
+        on_delete=models.CASCADE,
+        related_name="pagamentos"
+    )
+
+    def __str__(self):
+        return f"Pagamento {self.pagamento_id} - Reserva {self.reserva.reserva_id}"
