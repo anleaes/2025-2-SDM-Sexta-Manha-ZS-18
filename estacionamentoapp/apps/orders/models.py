eaa51d802fa7fc62e38ac3aabe8fe75ed528a017
Vinicius Barreto <viniciusbarretodasilva84@gmail.com>
2025-11-21 08:12:11 -0300

    Adiciona classe Reserva ao app orders

--- estacionamentoapp/apps/orders/models.py
+++ estacionamentoapp/apps/orders/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from clients.models import Client
+from products.models import Vaga
+from clients.models import Veiculo, Client
+
 # Create your models here.
 class Order(models.Model):
     payment_method = models.CharField('Forma de Pagamento', max_length=30, choices=[
@@ -13,7 +16,7 @@ class Order(models.Model):
         ('cancelado', 'Cancelado'),
     ])
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    
+
     class Meta:
         verbose_name = 'Pedido'
         verbose_name_plural = 'Pedidos'
@@ -21,3 +24,30 @@ class Order(models.Model):
 
     def __str__(self):
         return "%s" % (self.client) 
+    
+class Reserva(models.Model):
+    reserva_id = models.AutoField(primary_key=True)
+    data_inicio = models.DateTimeField()
+    data_fim = models.DateTimeField()
+    aprovado = models.BooleanField(default=False)
+
+    vaga = models.ForeignKey(
+        Vaga,
+        on_delete=models.CASCADE,
+        related_name="reservas"
+    )
+
+    veiculo = models.ForeignKey(
+        Veiculo,
+        on_delete=models.CASCADE,
+        related_name="reservas"
+    )
+
+    cliente = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        related_name="reservas"
+    )
+
+    def __str__(self):
+        return f"Reserva {self.reserva_id} - Cliente {self.cliente.first_name}"
